584b6f8e38ece623d7d76278a99fe35e2090f30e
Andre <afelipp3@gmail.com>
2021-10-05 11:36:13 -0300

    add fonts and global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,20 +1,65 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+
+  @font-face {
+    font-family: 'Gotham';
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+    src: local('Gotham Light'),
+        url('/fonts/gotham-bold.woff2') format('woff2'),
+  }
+
+  @font-face {
+    font-family: 'Gotham';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local('Gotham Book'),
+        url('/fonts/gotham-book.woff2') format('woff2'),
+  }
+
+  @font-face {
+    font-family: 'Gotham';
+    font-style: normal;
+    font-weight: 600;
+    src: local('Gotham Bold'),
+        url('/fonts/gotham-light.woff2') format('woff2'),
+  }
+
+  @font-face {
+    font-family: 'Gotham';
+    font-style: normal;
+    font-weight: 700;
+    font-display: swap;
+    src: local('Gotham Black'),
+        url('/fonts/gotham-black.woff2') format('woff2'),
+    }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
-  html {
-    font-size: 62.5%;
-  }
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
 
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+
+      background-color: ${theme.colors.mainBg};
+    }
+  `}
+
+
 `
 
 export default GlobalStyles
