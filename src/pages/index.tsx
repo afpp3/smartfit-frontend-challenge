1454fe750db051d4f987407dfa7125b5cec6c167
Andre <afelipp3@gmail.com>
2021-10-05 19:10:07 -0300

    add Heading in Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
 import Header from 'components/Header'
+import Heading from 'components/Heading'
 
 export default function Home() {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <div style={{ maxWidth: '70vw', margin: '0 auto' }}>
+        <Heading
+          title="REABERTURA SMART FIT"
+          description="O horário de funcionamento das nossas unidades está seguindo os
+          decretos de cada município. Por isso, confira aqui se a sua unidade está
+          aberta e as medidas de segurança que estamos seguindo."
+        />
+      </div>
+    </>
+  )
 }
